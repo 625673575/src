99f42ef71b5b5a3b40618b9aa901e61f4b5f7c69
frozen <625673575@qq.com>
2019-08-12 16:34:42 +0400

    了

--- CallDll/main.go
+++ CallDll/main.go
@@ -1,30 +1,102 @@
 package main
-import ("fmt"
+
+import (
+	"fmt"
+	"log"
 	"syscall"
 	"unsafe"
 )
+
 var (
-	testdll               = syscall.NewLazyDLL("TestDLL.dll")
-	procSetArray            = testdll.NewProc("SetArray")
-	procAddMoney              = testdll.NewProc("AddMoney")
-	procGetValueRef             = testdll.NewProc("GetValueRef")
-	procGetArray             = testdll.NewProc("GetArray")
-	procGetValue             = testdll.NewProc("GetValue")
+	testdll         = syscall.NewLazyDLL("TestDLL.dll")
+	procSetArray    = testdll.NewProc("SetArray")
+	procAddMoney    = testdll.NewProc("AddMoney")
+	procGetValueRef = testdll.NewProc("GetValueRef")
+	procGetArray    = testdll.NewProc("GetArray")
+	procGetValue    = testdll.NewProc("GetValue")
 )
 
 type ByteRet *byte
-func main(){
-	var i int64 =12
+
+func Test0() {
+	var i int64 = 12
 	//ip:=&i
 	fmt.Println(procAddMoney.Call())
-retInputPointer,_,_:=	procGetValueRef.Call(uintptr(unsafe.Pointer(&i)))
+	retInputPointer, _, _ := procGetValueRef.Call(uintptr(unsafe.Pointer(&i)))
 	fmt.Println(retInputPointer)
-	ar:=[]byte{1,2,4,3}
-	procSetArray.Call(uintptr(unsafe.Pointer(&ar)),uintptr(4))
-retGetArray,_,_:=	procGetArray.Call()
-ps:=	unsafe.Pointer(retGetArray+unsafe.Sizeof(ar[0]))
-bret:=	ByteRet(ps)
+	ar := []byte{1, 2, 4, 3}
+	procSetArray.Call(uintptr(unsafe.Pointer(&ar)), uintptr(4))
+	retGetArray, _, _ := procGetArray.Call()
+	ps := unsafe.Pointer(retGetArray + unsafe.Sizeof(ar[0]))
+	bret := ByteRet(ps)
 	fmt.Println(*bret)
-vret,_,_:=	procGetValue.Call(uintptr(1))
+	vret, _, _ := procGetValue.Call(uintptr(1))
 	fmt.Println(vret)
+}
+
+var (
+	throwdll        = syscall.NewLazyDLL("ThrowDll.dll")
+	LiterallyThrow  = throwdll.NewProc("LiterallyThrow")
+	CatchIndexError = throwdll.NewProc("CatchIndexError")
+	RunIndexError   = throwdll.NewProc("RunIndexError")
+	NoError         = throwdll.NewProc("NoError")
+)
+
+func Test1() {
+	NoError.Call()
+	//LiterallyThrow.Call()
+	//CatchIndexError.Call()
+	//RunIndexError.Call()
+}
+
+var (
+	ddzdll         = syscall.NewLazyDLL("ddz.dll")
+	RobotPlay      = ddzdll.NewProc("RobotPlay")
+	DealSmoothCard = ddzdll.NewProc("DealSmoothCard")
+	Add            = ddzdll.NewProc("Add")
+	AddBytes 	   = ddzdll.NewProc("AddBytes")
+)
+
+func TestRobotPlay() []byte {
+	ret := make([]byte, 20)
+	p0 := []byte{0x14, 0x24, 0x16, 0x48}
+	p1 := []byte{0x1}
+	p2 := []byte{0x2}
+	last := []byte{0x2f}
+	lastPlayIdentity := byte(0)
+	curPlayIdentity := byte(0)
+	l0 := byte(4)
+	l1 := byte(1)
+	l2 := byte(1)
+	RobotPlay.Call(uintptr(unsafe.Pointer(&ret[0])),uintptr(l0), uintptr(l1), uintptr(l2), uintptr(unsafe.Pointer(&p0[0])),
+		uintptr(unsafe.Pointer(&p1[0])),
+		uintptr(unsafe.Pointer(&p2[0])),
+		uintptr(l2), uintptr(unsafe.Pointer(&last[0])),
+		uintptr(lastPlayIdentity), uintptr(curPlayIdentity))
+	log.Println(ret)
+	return ret
+}
+
+func TestDealCard() {
+	desk := make([]byte, 3)
+	p0 := make([]byte, 17)
+	p1 := make([]byte, 17)
+	p2 := make([]byte, 17)
+	DealSmoothCard.Call(uintptr(unsafe.Pointer(&p0[0])), uintptr(unsafe.Pointer(&p1[0])), uintptr(unsafe.Pointer(&p2[0])), uintptr(unsafe.Pointer(&desk[0])))
+	log.Println(p0)
+	log.Println(p1)
+	log.Println(p2)
+	log.Println(desk)
+}
+func TestAddBytes(){
+	x:=[]byte{15,16,18,12}
+	r,_,_:=AddBytes.Call(uintptr(unsafe.Pointer(&x[0])),4)
+	log.Println(r)
+}
+func main() {
+	a, _, _ := Add.Call(uintptr(5), uintptr(6))
+	log.Println(a)
+	TestAddBytes()
+	TestDealCard()
+	TestRobotPlay()
 }
---
+++ ddz/grpc/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"ddz"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	"sync"
+)
+
+var ip string = "47.75.241.11:50001"
+
+func GrpcClient_Robot() {
+	conn, err := grpc.Dial(ip, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("did not connect :", err.Error())
+	}
+	defer conn.Close()
+	c := ddz.NewRobotServiceClient(conn)
+
+	lordCard := []byte{0x33, 0x25, 0x45, 0x28, 0x29, 0x49, 0x39}
+	farmer1Card := []byte{0x23, 0x13, 0x43, 0x46, 0x26}
+	farmer2Card := []byte{0x44, 0x34, 0x14}
+
+	lastCards := []byte{0x1e, 0x1e, 0x13, 0x23, 0x33}
+	r, err := c.Play(context.Background(), &ddz.RobotRequest{
+		Playeridentity:  0,
+		LordHandcard:    lordCard,
+		Farmer1Handcard: farmer1Card,
+		Farmer2Handcard: farmer2Card,
+		LastIdentity:    2,
+		LastPlaycard:    lastCards,
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(r.Handcard)
+	}
+}
+
+var connDealCardService ddz.DealCardServiceClient
+
+func GrpcClient_DealCard() {
+	r, err := connDealCardService.GetCard(context.Background(), &ddz.DealCardRequest{Params: []byte{0, 2}})
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(r.Player0, r.Player1, r.Player2, r.Extra)
+	}
+}
+func GrpcClient_Trustship() {
+	conn, err := grpc.Dial(ip, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("did not connect :", err.Error())
+	}
+	defer conn.Close()
+	c := ddz.NewTrustshipServiceClient(conn)
+
+	r, err := c.Ship(context.Background(), &ddz.TrustShipRequest{
+		PlayerIdentity: 1,
+		PlayerHandcard: []byte{0x44, 0x34, 0x14},
+		LastIdentity:   1,
+		LastPlaycard:   []byte{0x1e, 0x1e, 0x13, 0x23, 0x33},
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(r.Handcard)
+	}
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	GrpcClient_Trustship()
+	GrpcClient_Robot()
+	conn, err := grpc.Dial(ip, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("did not connect :", err.Error())
+	}
+	defer conn.Close()
+	connDealCardService = ddz.NewDealCardServiceClient(conn)
+	i := 0
+	for ; ;
+	{
+		i++
+		log.Println(i)
+		GrpcClient_DealCard()
+	}
+}
